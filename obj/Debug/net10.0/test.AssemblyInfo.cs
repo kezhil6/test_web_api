d5f841accbd509d62456add5bb6ad54dbcc503a8
kezhil6 <kezhil6@gmail.com>
2025-12-03 01:45:49 +0530

    Dockerfile.txt added

--- obj/Debug/net10.0/test.AssemblyInfo.cs
+++ obj/Debug/net10.0/test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf9167835add368497e36f6aad371ccaa3dc11a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
